3a4f48b1d69060dacaffd22812faa9f1491f2d8e
ale <ale7cosch@gmail.com>
2020-05-31 18:04:13 -0400

    test with more digits

--- app.rb
+++ app.rb
@@ -3,13 +3,16 @@ require './lib/torosVacas'
 get '/' do
   erb :showOptions
 end
+
 get '/ingresarCodigo' do
   erb :mainview
 end
+
 get '/iniciar' do
   $codigoSecreto=params[:codigo]
   erb :jugarview
 end
+
 post '/verificar' do
   $torosVacas=TorosVacas.new
   $intento=params[:intento]
--- spec/torosVacas_spec.rb
+++ spec/torosVacas_spec.rb
@@ -3,25 +3,38 @@ RSpec.describe TorosVacas do
     @@pruebas=TorosVacas.new
     it "devuelve true si el codigo ingresado es un numero" do
        
-        expect(@@pruebas.es_numero(1)).to eq(true)
-      end
-      it "devuelve true si el codigo ingresado es un string" do
-        
-        expect(@@pruebas.es_numero('1')).to eq(true)
-      end
-      it "devuelve true si el codigo ingresado es un cadena" do
-        expect(@@pruebas.es_numero('1234')).to eq(true)
-      end
+      expect(@@pruebas.es_numero(1)).to eq(true)
+    end
+      
+    it "devuelve true si el codigo ingresado es un string" do  
+      expect(@@pruebas.es_numero('1')).to eq(true)
+    end
+      
+    it "devuelve true si el codigo ingresado es un cadena" do
+      expect(@@pruebas.es_numero('1234')).to eq(true)
+    end
     
     it "Devuelve 1 toro si el codigo es 1 y el intento es 1 " do
       expect(@@pruebas.revisar([1],[1])).to eq([1,0])
     end
+    
     it "devuelve 1 vaca si el codigo es 5,7 y el intento 7,2 " do
         expect(@@pruebas.revisar([5,7],[7,2])).to eq([0,1])
     end
-    it "devuelve el string 12 en arreglo [1,2] " do
-        expect(@@pruebas.convertirStringArreglo("12")).to eq([1,2])
+
+    it "devuelve 1 vaca y 1 toro si el codigo es 1,2,3 y el intento 3,2,4 " do
+      expect(@@pruebas.revisar([1,2,3],[3,2,4])).to eq([1,1])
     end
+
+    it "devuelve 4 toros si el codigo es 1,2,3,4 y el intento 1,2,3,4" do
+      expect(@@pruebas.revisar([1,2,3,4],[1,2,3,4])).to eq([4,0])
+    end
+
+    it "devuelve el string 12 en arreglo [1,2] " do
+      expect(@@pruebas.convertirStringArreglo("12")).to eq([1,2])
+    end
+
+    
   end
 
 

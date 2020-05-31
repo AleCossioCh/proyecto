b2f6a1599c5839afcf9e145dd0cb5028902bb9bf
Pintex2 <Andreumark@hotmail.com>
2020-05-31 12:04:52 -0400

    basic secret code

--- lib/torosVacas.rb
+++ lib/torosVacas.rb
@@ -1,6 +1,6 @@
 class TorosVacas
     def es_numero(num)
-        return num.to_s
+        return num.to_s == num.to_i.to_s
     end
 
 end
--- spec/torosVacas_spec.rb
+++ spec/torosVacas_spec.rb
@@ -1,7 +1,17 @@
 require 'torosVacas.rb'
 RSpec.describe TorosVacas do
-    it 'devuelva el numero ingresado ' do
-        juego=TorosVacas.new
-        expect(juego.es_numero(1)).to eq('1')
-    end
+    it "devuelve true si el codigo ingresado es un numero" do
+        pruebas=TorosVacas.new
+        expect(pruebas.es_numero(1)).to eq(true)
+      end
+      it "devuelve true si el codigo ingresado es un string" do
+        pruebas=TorosVacas.new
+        expect(pruebas.es_numero('1')).to eq(true)
+      end
+      it "devuelve true si el codigo ingresado es un cadena" do
+        pruebas=TorosVacas.new
+        expect(pruebas.es_numero('1234')).to eq(true)
+      end
+      
+    
 end

914a8d23fd8060c982dada8a0c9196e5edf70dd6
bernardoclaure <63417312+bernardoclaure@users.noreply.github.com>
2020-05-31 16:29:55 -0400

    completed the function revisar

--- lib/torosVacas.rb
+++ lib/torosVacas.rb
@@ -4,16 +4,19 @@ class TorosVacas
     end
 
     def revisar(respuesta,codigo)
-        vacas=0
-        toros=0
-       
-            if respuesta==codigo
-              toros+=1
-            end
-        
-        
-        res=[toros,vacas]
-        return res
-      end
+      vacas=0
+     toros=0
+        for i in 0..codigo.length-1
+         for y in 0..codigo.length-1
+           if respuesta[i]==codigo[y] && i==y
+            toros+=1
+         elsif respuesta[i]==codigo[y]
+             vacas+=1
+         end
+        end
+     end
+    res=[toros,vacas]
+    return res
+  end
 
 end
